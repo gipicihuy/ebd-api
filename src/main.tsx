0424bd65014992aefa21bfa19dd4650792315d9d
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2025-10-11 21:47:03 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Index from './Index.tsx'; // Komponen utama Anda bernama Index
-import './index.css'; // <<< BARIS KRITIS YANG HILANG ATAU TERLEWAT
+import '/index.css'; // <<< BARIS KRITIS YANG HILANG ATAU TERLEWAT
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
